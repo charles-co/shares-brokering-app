4bddedc381fa05cb5f42e5d31cba0cfd9e6c095a
Charles Oraegbu <charlesboy49@gmail.com>
2022-03-15 23:47:46 +0100

    update

--- app/core/cron.py
+++ app/core/cron.py
@@ -1,18 +1,13 @@
-from apscheduler.executors.pool import ThreadPoolExecutor
+from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz import utc
 
 from app.api.utils import load_curreny
 
-
-def hello():
-    print("hello")
-
-
 jobstores = {"default": SQLAlchemyJobStore(url="sqlite:///jobs.sqlite")}
-executors = {"default": ThreadPoolExecutor(20)}
-job_defaults = {"coalesce": False, "max_instances": 3}
+executors = {"default": ThreadPoolExecutor(20), "processpool": ProcessPoolExecutor(5)}
+job_defaults = {"coalesce": True, "max_instances": 3}
 
 scheduler = BackgroundScheduler(jobstores=jobstores, executors=executors, timezone=utc)
 scheduler.add_job(
