5596c2a0b66bf685978fedf5e3327526b19c8086
Charles Oraegbu <charlesboy49@gmail.com>
2022-03-18 07:50:52 +0100

    fix cors

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ async def pong():
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
